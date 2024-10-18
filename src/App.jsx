e2e82d2f1ad581c6a83a3c1a006e7ed31302a2f1
HP <arunagiri9062@mountzion.ac.in>
2024-10-18 14:22:25 +0530

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,16 @@ import Register from './Pages/Register/Register'
 import Result from './Pages/Result/Result'
 
 function App() {
-  const [count, setCount] = useState(0)
+        const [count, setCount] = useState(0)
 
-  return (
-    <div>
-        {/* <Config/> */}
-        <Register/>
-        {/* <Dashboard /> */}
-        {/* <Result/> */}
-    </div>
-  )
+        return (
+                <div>
+                        {/* <Config/> */}
+                        {/* <Register/> */}
+                        <Dashboard />
+                        {/* <Result/> */}
+                </div>
+        )
 }
 
 export default App
